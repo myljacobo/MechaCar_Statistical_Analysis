f4d454266413cbfbf40b976aa8bfebc4188e0178
myljacobo <68582600+myljacobo@users.noreply.github.com>
2020-12-21 02:42:40 -0600

    adding updated rscript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,15 @@ suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names = F,stringsAs
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
 lot_summary <-suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+t.test(suspension_coil$PSI, mu=1500)
+
+lot_1 <- t.test(subset(suspension_coil, Manufacturing_Lot=='Lot1')$PSI, mu=1500)
+lot_1
+
+lot_2 <- t.test(subset(suspension_coil, Manufacturing_Lot=='Lot2')$PSI, mu=1500)
+lot_2
+
+lot_3 <- t.test(subset(suspension_coil, Manufacturing_Lot=='Lot3')$PSI, mu=1500)
+lot_3
+
